112bf9b38a0463684f7c7e182a6e9bbabb1f019b
Amichia Frejus Arnaud Aka <frejusarnaud@gmail.com>
2019-03-04 17:10:12 +0000

    Finalisation de la V12

--- url_access_restriction/static/src/js/url_access_restriction.js
+++ url_access_restriction/static/src/js/url_access_restriction.js
@@ -2,15 +2,19 @@ odoo.define("url_access_restriction.WarningMessage", function (require) {
   "use strcit";
 
   var core = require('web.core');
-  var Widget = require('web.Widget');
+  var AbstractAction = require('web.AbstractAction');
   var ControlPanelMixin = require('web.ControlPanelMixin');
 
-  var WarningMessage = Widget.extend(ControlPanelMixin, {
+  var WarningMessage = AbstractAction.extend(ControlPanelMixin, {
     'template': 'WarningMessageTemplate',
     init: function(parent, context) {
       this._super.apply(this, arguments);
-      $('.o_control_panel').addClass('o_hidden');
-    }
+      $(".o_control_panel").addClass('oe_hidden');
+    },
+    destroy: function () {
+      $(".o_control_panel").removeClass('oe_hidden');
+      return this._super.apply(this, arguments);
+    },
   });
 
   core.action_registry.add('warning_message', WarningMessage);
